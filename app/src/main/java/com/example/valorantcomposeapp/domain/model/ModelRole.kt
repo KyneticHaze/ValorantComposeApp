d101ec08be1ca740d2c746db2102d9ee32994eb9
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-10 13:38:08 +0300

    feat: role data added :sparkles:

---
+++ app/src/main/java/com/example/valorantcomposeapp/domain/model/ModelRole.kt
@@ -0,0 +1,9 @@
+package com.example.valorantcomposeapp.domain.model
+
+data class ModelRole(
+    val assetPath: String,
+    val description: String,
+    val displayIcon: String,
+    val displayName: String,
+    val uuid: String
+)
