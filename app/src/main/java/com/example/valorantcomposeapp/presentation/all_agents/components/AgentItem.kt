0e577b4034cf69de0d63edd4d6eb07a395f4be3b
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-11 00:35:17 +0300

    feat: AgentItem added :sparkles:

---
+++ app/src/main/java/com/example/valorantcomposeapp/presentation/all_agents/components/AgentItem.kt
@@ -0,0 +1,69 @@
+package com.example.valorantcomposeapp.presentation.all_agents.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.valorantcomposeapp.domain.model.Agent
+
+@Composable
+fun AgentItem(
+    agent: Agent,
+    navigate: (String) -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .clickable { navigate(agent.uuid) }
+            .padding(12.dp)
+            .background(Color.LightGray)
+    ) {
+        Box {
+            AsyncImage(
+                model = agent.displayIcon,
+                contentDescription = agent.description,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(130.dp)
+            )
+            Text(
+                text = agent.displayName,
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.White,
+                fontWeight = FontWeight.SemiBold,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .background(brush = Brush.verticalGradient(
+                        listOf(
+                            Color.Transparent,
+                            Color.Gray
+                        )
+                    ))
+                    .padding(4.dp)
+                    .fillMaxWidth()
+            )
+        }
+    }
+}
