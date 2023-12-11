b8263e74ed7d6021dce2407d44d5e3e8a3af735c
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-11 19:44:22 +0300

    feat: new routes added :sparkles:

--- app/src/main/java/com/example/valorantcomposeapp/common/Routes.kt
+++ app/src/main/java/com/example/valorantcomposeapp/common/Routes.kt
@@ -1,6 +1,8 @@
 package com.example.valorantcomposeapp.common
 
-sealed class Routes(val routes : String) {
+sealed class Routes(val route : String) {
     data object Agents : Routes("agents")
-    data object AgentDetail : Routes("agent")
+    data object Maps : Routes("maps")
+    data object Weapons : Routes("weapons")
+    data object Tiers : Routes("tiers")
 }
