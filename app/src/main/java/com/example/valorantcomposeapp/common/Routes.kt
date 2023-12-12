639b7ddd6f4cee62e9525230dea15b89dbe93b61
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-12 23:49:13 +0300

    feat: detail route added :sparkles:

--- app/src/main/java/com/example/valorantcomposeapp/common/Routes.kt
+++ app/src/main/java/com/example/valorantcomposeapp/common/Routes.kt
@@ -2,7 +2,9 @@ package com.example.valorantcomposeapp.common
 
 sealed class Routes(val route : String) {
     data object Agents : Routes("agents")
+    data object AgentDetail : Routes("agent_detail")
     data object Maps : Routes("maps")
+    data object MapDetail : Routes("map_detail")
     data object Weapons : Routes("weapons")
     data object Tiers : Routes("tiers")
 }
