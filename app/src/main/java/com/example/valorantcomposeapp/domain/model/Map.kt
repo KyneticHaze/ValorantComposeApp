895bb66d043c35151e41ab34e89073f088fee050
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-12 23:28:37 +0300

    feat: map model created :sparkles:

---
+++ app/src/main/java/com/example/valorantcomposeapp/domain/model/Map.kt
@@ -0,0 +1,11 @@
+package com.example.valorantcomposeapp.domain.model
+
+data class Map(
+    val uuid : String?,
+    val displayName : String?,
+    val description : String?,
+    val coordinates : String?,
+    val displayMap : String?,
+    val displayLittleMap : String?,
+    val mapImage : String?
+)
