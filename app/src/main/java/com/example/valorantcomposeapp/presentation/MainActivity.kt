e92e2000123a1a6e2e788b5936229c2127a3d614
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-12 23:52:04 +0300

    refactor: unnecessary import path deleted :package:

--- app/src/main/java/com/example/valorantcomposeapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/valorantcomposeapp/presentation/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.valorantcomposeapp.presentation
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,13 +13,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.valorantcomposeapp.presentation.ui.theme.ValorantComposeAppTheme
-
+import dagger.hilt.android.AndroidEntryPoint
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ValorantComposeAppTheme {
-
+                ValorantApp()
             }
         }
     }
