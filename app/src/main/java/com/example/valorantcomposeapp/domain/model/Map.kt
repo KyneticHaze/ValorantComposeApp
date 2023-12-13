57c079f698499bf936afb0797bbc28f03142af62
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-13 18:51:14 +0300

    feat: mapSites value added to map model :sparkles:

--- app/src/main/java/com/example/valorantcomposeapp/domain/model/Map.kt
+++ app/src/main/java/com/example/valorantcomposeapp/domain/model/Map.kt
@@ -1,11 +1,12 @@
 package com.example.valorantcomposeapp.domain.model
 
 data class Map(
-    val uuid : String?,
-    val displayName : String?,
-    val description : String?,
-    val coordinates : String?,
-    val displayMap : String?,
-    val displayLittleMap : String?,
-    val mapImage : String?
+    val uuid : String,
+    val displayName : String,
+    val description : String,
+    val coordinates : String,
+    val displayMap : String,
+    val displayLittleMap : String,
+    val mapImage : String,
+    val mapSites : String
 )
