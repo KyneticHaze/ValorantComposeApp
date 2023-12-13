ed04a9fdf684ba087b9dbc8e247b68871829293f
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-13 13:31:48 +0300

    style: Navigation color modified :gem:

--- app/src/main/java/com/example/valorantcomposeapp/navigation/MainNavigationBar.kt
+++ app/src/main/java/com/example/valorantcomposeapp/navigation/MainNavigationBar.kt
@@ -1,54 +1,41 @@
 package com.example.valorantcomposeapp.navigation
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
-import com.example.valorantcomposeapp.common.Routes
-
-data class MainNavigationItem(
-    val title: String,
-    val selectedIcon: ImageVector,
-    val unSelectedIcon: ImageVector,
-    val route: String
-)
-
-val items = listOf(
-    MainNavigationItem(
-        title = "Agents",
-        selectedIcon = Icons.Filled.Home,
-        unSelectedIcon = Icons.Outlined.Home,
-        route = Routes.Agents.routes
-    ),
-    MainNavigationItem(
-        title = "Agent",
-        selectedIcon = Icons.Filled.Person,
-        unSelectedIcon = Icons.Outlined.Person,
-        route = Routes.AgentDetail.routes
-    )
-)
+import com.example.valorantcomposeapp.presentation.ui.theme.cupidEye
+import com.example.valorantcomposeapp.presentation.ui.theme.radiant
+import com.example.valorantcomposeapp.presentation.ui.theme.wildApothecary
 
 @Composable
 fun MainNavigationBar(
     navController: NavHostController
 ) {
 
+    val items = listOf(
+        NavItem.Agents,
+        NavItem.Maps,
+        NavItem.Weapons,
+        NavItem.Tiers
+    )
+
     var selectedItemIndex by rememberSaveable {
         mutableIntStateOf(0)
     }
 
-    NavigationBar {
+    NavigationBar(
+        containerColor = cupidEye
+    ) {
         items.forEachIndexed { index, item ->
             NavigationBarItem(
                 selected = selectedItemIndex == index,
@@ -61,13 +48,16 @@ fun MainNavigationBar(
                 },
                 icon = {
                     Icon(
-                        imageVector = if (index == selectedItemIndex) {
-                            item.selectedIcon
-                        } else {
-                            item.unSelectedIcon
-                        }, contentDescription = item.title
+                        painter = painterResource(id = item.icon),
+                        contentDescription = item.title,
                     )
-                }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = wildApothecary,
+                    unselectedIconColor = radiant,
+                    selectedTextColor = wildApothecary,
+                    unselectedTextColor = radiant
+                )
             )
         }
     }
