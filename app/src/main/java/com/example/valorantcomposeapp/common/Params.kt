175d61a17e5409c15b966a0f26db4232ea57d34a
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-15 12:41:34 +0300

    feat: weaponId added :sparkles:

--- app/src/main/java/com/example/valorantcomposeapp/common/Params.kt
+++ app/src/main/java/com/example/valorantcomposeapp/common/Params.kt
@@ -3,4 +3,5 @@ package com.example.valorantcomposeapp.common
 object Params {
     const val PARAM_AGENT_ID = "agentUuid"
     const val PARAM_MAP_ID = "mapUuid"
+    const val PARAM_WEAPON_ID = "weaponUuid"
 }
