34b1e0dfeae600abf38091a56f0ece2d13269670
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-15 16:07:58 +0300

    feat: Splash route and nav composable added :sparkles:

--- app/src/main/java/com/example/valorantcomposeapp/navigation/Navgraph.kt
+++ app/src/main/java/com/example/valorantcomposeapp/navigation/Navgraph.kt
@@ -14,6 +14,10 @@ import com.example.valorantcomposeapp.presentation.agents.agentDetail.AgentDetai
 import com.example.valorantcomposeapp.presentation.agents.all_agents.AgentsScreen
 import com.example.valorantcomposeapp.presentation.maps.all_maps.MapsScreen
 import com.example.valorantcomposeapp.presentation.maps.map_detail.MapDetailScreen
+import com.example.valorantcomposeapp.presentation.splash.SplashScreen
+import com.example.valorantcomposeapp.presentation.tiers.TiersScreen
+import com.example.valorantcomposeapp.presentation.weapons.weaponDetail.WeaponDetailScreen
+import com.example.valorantcomposeapp.presentation.weapons.all_weapons.WeaponsScreen
 
 @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 @Composable
@@ -23,9 +27,20 @@ fun NavGraphComponent(
 ) {
     NavHost(
         navController = navController,
-        startDestination = Routes.Agents.route,
+        startDestination = Routes.Splash.route,
         modifier = Modifier.padding(paddingValues)
     ) {
+
+        composable(Routes.Splash.route) {
+            SplashScreen {
+                navController.navigate(Routes.Agents.route) {
+                    popUpTo(Routes.Splash.route) {
+                        inclusive = true
+                    }
+                }
+            }
+        }
+
         composable(Routes.Agents.route) {
             AgentsScreen(
                 navigateToAgentDetail = {
@@ -46,6 +61,19 @@ fun NavGraphComponent(
         }
         composable(route = "${Routes.MapDetail.route}/{mapUuid}") {
             MapDetailScreen()
+        }
+        composable(route = Routes.Weapons.route) {
+            WeaponsScreen(
+                navigateToWeaponDetail = {
+                    navController.navigate("${Routes.WeaponsDetail.route}/$it")
+                }
+            )
+        }
+        composable(route = "${Routes.WeaponsDetail.route}/{weaponUuid}") {
+            WeaponDetailScreen()
+        }
+        composable(route = Routes.Tiers.route) {
+            TiersScreen()
         }
     }
 }
